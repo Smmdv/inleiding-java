1ad7c463e252002d15c4ea76123df9c44a272bb3
Smmdv <Semdevries01@gmail.com>
2020-11-16 09:02:40 +0100

    H2

---
+++ inleiding-java/src/h21.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+
+public class h21 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Sem", 50, 50 );
+    }
+}
